3c7c091afa184680f00a8e907dfc7aa9527e3225
Wang Lei <lei.wang1@thoughtworks.com>
2022-02-17 11:02:11 +0800

    add axios and update tests

--- test/ShoppingCart.spec.js
+++ test/ShoppingCart.spec.js
@@ -3,7 +3,12 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import ShoppingCart from '../src/pages/ShoppingCart';
 
-test('Main', () => {
-    const { getByText } = render(<ShoppingCart />);
-    expect(getByText('Shopping Cart')).toBeInTheDocument();
+describe('Shopping Cart', () => {
+    test('should show shopping cart page', () => {
+        const { getByText } = render(<ShoppingCart />);
+        expect(getByText('Shopping Cart')).toBeInTheDocument();
+        expect(getByText('商品名称')).toBeInTheDocument();
+        expect(getByText('单 价')).toBeInTheDocument();
+        expect(getByText('数 量')).toBeInTheDocument();
+    });
 });
