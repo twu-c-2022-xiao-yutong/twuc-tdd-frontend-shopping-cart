0683d10f773d9c04b987fafae276837b78db2f8d
Wang Lei <lei.wang1@thoughtworks.com>
2022-04-15 09:49:51 +0800

    feat: show products

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './index.css';
+import { getProducts } from './service';
 
 
 class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    getProducts().then(data => this.setState({ products: data }));
+  }
+
   render() {
     return (
       <div  className="wrapper">
@@ -15,6 +27,15 @@ class ShoppingCart extends React.Component {
               <th className="table-cell align-right">数量</th>
             </tr>
           </thead>
+          <tbody>
+          {this.state.products && this.state.products.map((product) => (
+              <tr key={product.id} className="table-row">
+                <td className="table-cell align-left">{product.name}</td>
+                <td className="table-cell align-right">{product.price.toFixed(2)}</td>
+                <td className="table-cell align-right">{product.count}</td>
+              </tr>
+          ))}
+          </tbody>
         </table>
       </div>
     );
--- src/pages/shoppingCart/test/index.test.js
+++ src/pages/shoppingCart/test/index.test.js
@@ -1,9 +1,28 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 import ShoppingCart from '../index';
+import { getProducts } from '../service';
+
+jest.mock('../service');
 
 describe('Shopping Cart', () => {
+  beforeEach(() => {
+    // given
+    getProducts.mockResolvedValueOnce(
+        [
+          {"id": 1, "name": "小米手环", "price": 299.00, "count": 1},
+          {"id": 2, "name": "任天堂 Nintendo Switch", "price": 2099.00, "count": 2},
+          {"id": 3, "name": "SONY WH-1000XM4 无线智能降噪耳机", "price": 2099.00, "count": 1},
+          {"id": 4, "name": "iPhone 13 256GB", "price": 6799.00, "count": 1}
+        ]
+    );
+  });
+
+  afterEach(() => {
+    getProducts.mockReset();
+  });
+
   test('should show shopping cart page', () => {
     const { getByText } = render(<ShoppingCart/>);
 
@@ -22,5 +41,31 @@ describe('Shopping Cart', () => {
     ['商品名称', '单价', '数量'].forEach((text, index) => {
       expect(thElements.item(index)).toHaveTextContent(text);
     })
+  })
+
+  test('should show products in product list', async () => {
+
+    // when
+    const { container } = render(<ShoppingCart/>);
+
+    // then
+    await waitFor(() => {
+      const trElements = container.querySelectorAll('tbody tr');
+      expect(trElements.length).toBe(4);
+
+      const tableContent = [
+        ['小米手环', '299.00', '1'],
+        ['任天堂 Nintendo Switch', '2099.00', '2'],
+        ['SONY WH-1000XM4 无线智能降噪耳机', '2099.00', '1'],
+        ['iPhone 13 256GB', '6799.00', '1'],
+      ];
+      for (let i = 0; i < trElements.length; i++) {
+        const tdElements = trElements.item(i).getElementsByTagName('td');
+        for (let j = 0; j < tdElements.length; j++) {
+          expect(tdElements.item(j)).toHaveTextContent(tableContent[i][j]);
+        }
+      }
+    })
+
   })
 });
