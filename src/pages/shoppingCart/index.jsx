60f2ddef20db986afe459a711a6c6687232c1f92
Sisi Yang <sisi.yang@thoughtworks.com>
2022-08-11 10:13:55 +0800

    feat: Show empty cart

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 const ShoppingCart = () => {
   return (
     <div className="wrapper">
       <div className="title">Shopping Cart</div>
+
+      <table className="table">
+        <thead className="table-header">
+          <tr className="table-row">
+            <th className="table-cell align-left">商品名称</th>
+            <th className="table-cell align-left">单价</th>
+            <th className="table-cell align-left">数量</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };
-
 
 export default ShoppingCart;
--- src/pages/shoppingCart/test/index.test.js
+++ src/pages/shoppingCart/test/index.test.js
@@ -1,12 +1,23 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import React from 'react';
-import ShoppingCart from '../index';
+import { getByText, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import ShoppingCart from "../index";
 
-describe('Shopping Cart', () => {
-  test('should show shopping cart page', () => {
-    const { getByText } = render(<ShoppingCart/>);
+describe("Shopping Cart", () => {
+  test("should show shopping cart page", () => {
+    const { getByText } = render(<ShoppingCart />);
 
-    expect(getByText('Shopping Cart')).toBeInTheDocument();
+    expect(getByText("Shopping Cart")).toBeInTheDocument();
+  });
+
+  test("should show empty cart, given product list is empty", () => {
+    const { getByText, container } = render(<ShoppingCart />);
+
+    const products = container.getElementsByClassName("product");
+
+    expect(getByText("商品名称")).toBeTruthy();
+    expect(getByText("单价")).toBeTruthy();
+    expect(getByText("数量")).toBeTruthy();
+    expect(products).toHaveLength(0);
   });
 });
