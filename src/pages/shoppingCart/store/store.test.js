6e00be7e5ef7be049deb3be01f3e2e0d4e7fcc28
Sisi Yang <sisi.yang@thoughtworks.com>
2022-08-11 11:42:49 +0800

    refactor: Extract product list file in mockData

--- src/pages/shoppingCart/store/store.test.js
+++ src/pages/shoppingCart/store/store.test.js
@@ -1,20 +1,11 @@
 import { getData } from "../../../service/service";
 import { getProducts } from "./store";
+import products from "../../../mockData/products.json";
+
 jest.mock("../../../service/service");
 
 describe("Store", () => {
   test("Should get product list when call getProducts", async () => {
-    const products = [
-      { id: 1, name: "小米手环", price: 299, count: 1 },
-      { id: 2, name: "任天堂 Nintendo Switch", price: 2099, count: 2 },
-      {
-        id: 3,
-        name: "SONY WH-1000XM4 无线智能降噪耳机",
-        price: 2099,
-        count: 1,
-      },
-      { id: 4, name: "iPhone 13 256GB", price: 6799, count: 1 },
-    ];
     getData.mockResolvedValue({ products });
 
     const actual = await getProducts();
--- src/service/service.test.js
+++ src/service/service.test.js
@@ -1,28 +1,20 @@
 import axios from "axios";
 import { getData } from "./service";
+import { GET_DATA } from "./api";
+import products from "../mockData/products.json";
 
 jest.mock("axios");
 
 describe("Service", () => {
   test("should send request to get data when call getData", async () => {
     const data = {
-      products: [
-        { id: 1, name: "小米手环", price: 299, count: 1 },
-        { id: 2, name: "任天堂 Nintendo Switch", price: 2099, count: 2 },
-        {
-          id: 3,
-          name: "SONY WH-1000XM4 无线智能降噪耳机",
-          price: 2099,
-          count: 1,
-        },
-        { id: 4, name: "iPhone 13 256GB", price: 6799, count: 1 },
-      ],
+      products,
     };
     axios.get.mockResolvedValue({ data });
 
     const actual = await getData();
 
     expect(actual).toEqual(data);
-    expect(axios.get).toBeCalledWith("http://localhost:8000/");
+    expect(axios.get).toBeCalledWith(GET_DATA);
   });
 });
