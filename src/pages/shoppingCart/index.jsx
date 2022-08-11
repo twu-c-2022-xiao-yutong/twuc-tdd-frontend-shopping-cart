933c09b1777d40eb9a79653beee831e6721c0360
YutongXiao <annayutong@gmail.com>
2022-08-11 10:14:10 +0800

    feat: show empty cart

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -5,6 +5,16 @@ const ShoppingCart = () => {
   return (
     <div className="wrapper">
       <div className="title">Shopping Cart</div>
+
+      <table className="table">
+        <thead className="table-header">
+          <tr className="table-row">
+            <th className="table-cell align-left">商品名称</th>
+            <th className="table-cell align-left">单价</th>
+            <th className="table-cell align-left">数量</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };
--- src/pages/shoppingCart/test/index.test.js
+++ src/pages/shoppingCart/test/index.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import {getByText, render} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 import ShoppingCart from '../index';
@@ -8,5 +8,16 @@ describe('Shopping Cart', () => {
     const { getByText } = render(<ShoppingCart/>);
 
     expect(getByText('Shopping Cart')).toBeInTheDocument();
+  });
+
+  test('should show empty cart, given product list is empty', () => {
+    const { getByText, container } = render(<ShoppingCart/>);
+
+    const products = container.getElementsByClassName('product');
+
+    expect(getByText("商品名称")).toBeTruthy();
+    expect(getByText("单价")).toBeTruthy();
+    expect(getByText("数量")).toBeTruthy();
+    expect(products).toHaveLength(0);
   });
 });
