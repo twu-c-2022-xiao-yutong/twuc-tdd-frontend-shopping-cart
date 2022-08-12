00f0683e21528007f095a6a851102f1091cf6e20
YutongXiao <annayutong@gmail.com>
2022-08-12 09:21:26 +0800

    feat: extract product list data

---
+++ src/service/api.js
@@ -0,0 +1,1 @@
+export const GET_DATA = 'http://localhost:8000/';
--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import {GET_DATA} from './api';
 
 export const getData = async () => {
-  const response = await axios.get('http://localhost:8000/');
+  const response = await axios.get(GET_DATA);
   return response.data;
 };
